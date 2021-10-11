cb6178865edb5e4d34827cfbd078d66900d04a17
mailinnmoneel <mailinn.kiste@gmail.com>
2021-10-11 11:10:20 +0200

    Initial commit

---
+++ Slektstre/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slektstre
+{
+    class Person
+    {
+        string fornavn = "fornavn";
+        string etternavn = "etternavn";
+        int fødselsår = 2000;
+        int dødsår = 2080;
+    }
+}
---
+++ Slektstre/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slektstre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kommando
+            //hjelp
+            //liste
+            //vis id
+        }
+    }
+}
